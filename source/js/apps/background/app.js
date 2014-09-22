52d533cb640d3a524271fac5875a17bc546d182d
Mike Green <mike.is.green@gmail.com>
2014-09-22 10:35:54 -0400

    Wrote background application bootstrap code

--- source/js/apps/background/app.js
+++ source/js/apps/background/app.js
@@ -3,13 +3,24 @@
  * Background Application Module
  */
 
+/* global chrome */
+
 (function(window, undefined) {
 
   'use strict';
 
-  var $ = require('jquery');
   var storage = require('../../lib/storage');
   var BackgroundApplication = require('./lib/background-application');
+
+  storage.get(['subdomain', 'authString']).then(function(items) {
+    if (items.subdomain && items.authString) {
+      window.application = new BackgroundApplication(items.subdomain, items.authString);
+    } else {
+      window.application = new BackgroundApplication(false, false);
+      chrome.browserAction.setBadgeText({ text: '!!!' });
+      console.error('Error initializing Hayfever. Please visit options page.');
+    }
+  });
 
 
 })(window);
