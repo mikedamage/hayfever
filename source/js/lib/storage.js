735d1dca67152ff52f7234bd6921bce4c1ec3ddc
Mike Green <mike.is.green@gmail.com>
2014-09-21 21:22:10 -0400

    Finished async promise based storage adapter

--- source/js/lib/storage.js
+++ source/js/lib/storage.js
@@ -1,7 +1,9 @@
 /**
  * Hayfever for Harvest
- * Chrome Storage Module
+ * Promise-based Chrome Storage Module
  */
+
+/* global chrome */
 
 (function(window, undefined) {
 
@@ -10,11 +12,44 @@
   var Q = require('q');
 
   var storage = {
-    get: function(key) {
+    get: function get(keys) {
+      var deferred = Q.defer();
 
+      chrome.storage.local.get(keys, function(vals) {
+        deferred.resolve(vals);
+      });
+
+      return deferred.promise;
     },
-    set: function(key, val) {
 
+    set: function set(vals) {
+      var deferred = Q.defer();
+
+      chrome.storage.local.set(vals, function() {
+        deferred.resolve(vals);
+      });
+
+      return deferred.promise;
+    },
+
+    getBytes: function getBytes(keys) {
+      var deferred = Q.defer();
+
+      chrome.storage.local.getBytesInUse(keys, function(bytes) {
+        return deferred.resolve(bytes);
+      });
+
+      return deferred.promise;
+    },
+
+    remove: function remove(keys) {
+      var deferred = Q.defer();
+
+      chrome.storage.local.remove(keys, function() {
+        deferred.resolve(keys);
+      });
+
+      return deferred.promise;
     }
   };
 
